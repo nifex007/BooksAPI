0a4779748a86c2543bfdf763cedfc64724292b47
Nifemi Sola-Ojo <nifemi@ashnelsonpartners.com>
2019-08-19 14:32:16 +0100

    added query

--- app.js
+++ app.js
@@ -2,20 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const db = mongoose.connect('mongodb://localhost/bookAPI', {useNewUrlParser: true});
+const db = mongoose.connect('mongodb://localhost/bookAPI', {
+  useNewUrlParser: true
+});
 const booksRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
 
 booksRouter.route('/books')
   .get((req, res) => {
-    // const response = { holla : 'This is the API'};
-    Book.find((err, books) => {
+    const { query } = req;
+    Book.find(query, (err, books) => {
       if (err) {
         return res.send(err);
       }
-      return res.json(books)
-    })
+      return res.json(books);
+    });
   });
 
 app.use('/api', booksRouter);
